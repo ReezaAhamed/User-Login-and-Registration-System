6afce1331c139199d8c957ae1c01d9bde83162ab
Reeza Ahamed <riiza.ahamed@gmail.com>
2022-01-19 15:03:52 +0100

    Confirmation token service

---
+++ user-registration-system/src/main/java/com/reeza/userregistration/registration/token/ConfirmationTokenService.java
@@ -0,0 +1,29 @@
+package com.reeza.userregistration.registration.token;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class ConfirmationTokenService {
+
+	/*
+	 * private final ConfirmationTokenRepository confirmationTokenRepository;
+	 * 
+	 * public void saveConfirmationToken(ConfirmationToken token) {
+	 * confirmationTokenRepository.save(token); }
+	 * 
+	 * public Optional<ConfirmationToken> getToken(String token){
+	 * 
+	 * return confirmationTokenRepository.findByToken(token); }
+	 * 
+	 * public int setConfirmedTime(String token) {
+	 * 
+	 * return confirmationTokenRepository.updateConfirmationTime(token,
+	 * LocalDateTime.now()); }
+	 */
+}
