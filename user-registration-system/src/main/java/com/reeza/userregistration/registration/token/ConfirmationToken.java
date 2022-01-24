6a24c23832e83565ebf2d63f705191026dedd924
Reeza Ahamed <riiza.ahamed@gmail.com>
2022-01-24 23:03:40 +0100

    Confirmation Token

--- user-registration-system/src/main/java/com/reeza/userregistration/registration/token/ConfirmationToken.java
+++ user-registration-system/src/main/java/com/reeza/userregistration/registration/token/ConfirmationToken.java
@@ -17,42 +17,42 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-/*
+
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
-@Table(name="confirmation_token")*/
+@Table(name="confirmation_token")
 public class ConfirmationToken {
 	
-	/*
-	 * @Id
-	 * 
-	 * @GeneratedValue(strategy = GenerationType.IDENTITY)
-	 * 
-	 * @Column(name ="id") private int id;
-	 * 
-	 * @Column(name="token", nullable = false) private String token;
-	 * 
-	 * @Column(name="created_time", nullable = false) private LocalDateTime
-	 * createdTime;
-	 * 
-	 * @Column(name="expired_time", nullable = false) private LocalDateTime
-	 * expirationTime; // we set
-	 * 
-	 * @Column(name="confirmed_time") private LocalDateTime confirmedTime;
-	 * 
-	 * @ManyToOne
-	 * 
-	 * @JoinColumn ( nullable = false, name = "app_user_id" ) private
-	 * ApplicationUser appUser;
-	 * 
-	 * 
-	 * public ConfirmationToken(String token, LocalDateTime createdTime,
-	 * LocalDateTime expirationTime, ApplicationUser appUser) { super(); this.token
-	 * = token; this.createdTime = createdTime; this.expirationTime =
-	 * expirationTime; this.appUser = appUser; }
-	 */
+	
+	  @Id
+	  
+	  @GeneratedValue(strategy = GenerationType.IDENTITY)
+	  
+	  @Column(name ="id") private int id;
+	  
+	  @Column(name="token", nullable = false) private String token;
+	  
+	  @Column(name="created_time", nullable = false) private LocalDateTime
+	  createdTime;
+	  
+	  @Column(name="expired_time", nullable = false) private LocalDateTime
+	  expirationTime; // we set
+	  
+	  @Column(name="confirmed_time") private LocalDateTime confirmedTime;
+	  
+	  @ManyToOne
+	  
+	  @JoinColumn ( nullable = false, name = "app_user_id" ) private
+	  ApplicationUser appUser;
+	  
+	  
+	  public ConfirmationToken(String token, LocalDateTime createdTime,
+	  LocalDateTime expirationTime, ApplicationUser appUser) { super(); this.token
+	  = token; this.createdTime = createdTime; this.expirationTime =
+	  expirationTime; this.appUser = appUser; }
+	 
 	
 	
 
